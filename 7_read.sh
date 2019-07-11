71e8455fb82a576653ba6fb761c1e69156d3e45c
Jose Luis <jlrc23@gmail.com>
2019-07-11 08:06:00 -0500

    add read

--- 7_read.sh
+++ 7_read.sh
@@ -1,19 +1,11 @@
 # ! /bin/bash
-# Programa para ejemplificar como capturar la informacion del usuario utilizabdo el comando echo, read y $REPL
+# Programa para ejemplificar como capturar la informacion del usuario utilizabdo el comando read
 # Aurtor: Jose Luis Rosado - @jlrc23
 
 option=0
 backupName=""
 
-echo "Programa utilizadades Postgres"
-echo -n "Ingresar una opcion":
-read
-option=$REPLY
-
-echo -n "Ingresar el nombre del archivo del backup: "
-read 
-backupName=$REPLY
-
+echo "Programa Utilidades Postgres"
+read -p "Ingresar una opción:" option
+read -p "Ingresa el nombre del archivo del backup:" backupName
 echo "Opcion: $option, backupName: $backupName"
-
-
