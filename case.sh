3036a1b558470baa44e1358981df5770a0e066c3
rojo7006 <jose.rosado.consultant@nielsen.com>
2019-07-11 16:24:43 -0500

    Add case

---
+++ case.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+case $1 in 
+  [Ss]tart)
+    echo "Iniciando el programa"
+    ;;
+  [Ss]top)
+    echo "Parando el programa"
+    ;;
+  help|*)
+    echo "Para usar el script usa stop | start"
+esac
+
